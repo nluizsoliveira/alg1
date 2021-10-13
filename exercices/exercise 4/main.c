74bfc0e42cfa201f427bdb7dfc49a919860358f2
nluizsoliveira <nluizsolveira@gmail.com>
2021-10-13 04:07:25 -0300

    feat(exercise-4): parse line in operation, first and second number

--- exercices/exercise 4/main.c
+++ exercices/exercise 4/main.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
-#include<readline.h>
-
+#include <readline.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main (){
     int operations_amount;
     scanf("%d ", &operations_amount);
-    for(int line_counter = 0; line_counter < operations_amount; line_counter++){
+    for(int counter = 0; counter < operations_amount; counter++){
+        char* operation;
+        char* first_number_buffer;
+        char* second_number_buffer;
+
         char* line = read_line();
-        printf("%s\n", line);
+        char *line_split = strtok(line,  " ");
+
+        operation = (char *) malloc(strlen(line_split) * sizeof(char));
+        strcpy(operation, line_split);
+
+        line_split = strtok(NULL, " ");
+        first_number_buffer = (char *) malloc(strlen(line_split) * sizeof(char));
+        strcpy(first_number_buffer, line_split);
+
+        line_split = strtok(NULL, " ");
+        second_number_buffer = (char *) malloc(strlen(line_split) * sizeof(char));
+        strcpy(second_number_buffer, line_split);
+
+        printf("%s %s %s\n", line,first_number_buffer,second_number_buffer);
+        
     }
 
 
+
+
 }
+
--- exercices/exercise 4/readline.c
+++ exercices/exercise 4/readline.c
@@ -7,12 +7,14 @@ char* read_line(){
     const char UNIX_LINEBREAK = '\n';
     const char WINDOWS_LINEBREAK = '\r';
     const char C_STRING_TERMINATOR = '\0';
+    
+    char extra_linebreak;
+    char current_letter;
 
     char* line = NULL;
-    char current_letter;
     int position = 0;
     bool reading_line = true;
-    char extra_linebreak;
+    
 
     while(reading_line){
         scanf("%c", &current_letter);
@@ -31,7 +33,7 @@ char* read_line(){
     }
 
     line = (char*) realloc(line, sizeof(char) * (position +  1));
-    line[position-1] = C_STRING_TERMINATOR;
+    line[position] = C_STRING_TERMINATOR;
 
     return line; 
 }
