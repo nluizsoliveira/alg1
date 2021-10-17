bac4e79426350bed04421eede30cfd8b1e1bc770
nluizsoliveira <nluizsoliveira@gmail.com>
2021-10-17 01:45:42 -0300

    feat(exercise 5): add game struct and new method

--- exercices/exercise 5/main.c
+++ exercices/exercise 5/main.c
@@ -3,21 +3,34 @@
 #include<stdbool.h>
 #include<string.h>
 
+typedef struct game{
+    char* name;
+    char* company;
+    int year; 
+} Game;
+
+Game* new_Game(char*company, char*name, int year){
+    Game* game = (Game*) malloc (sizeof(Game*));
+    strcpy(game->name, name);
+    strcpy(game->company, company);
+    game->year = year;
+    return game;
+}
+
 int main (){
     char csv_filepath[10]= "./CSV.csv";
     char mode[1] = "r";
 
     FILE* csv = fopen(csv_filepath, mode);
 
-    int counter = 1;
     char* line = read_line_from_file(csv);
-    printf("%d %s\n", counter, line);
+    bool is_line_valid = strlen(line);
 
-    while(strlen(line)){
+    while(is_line_valid){
         line = read_line_from_file(csv);
-        counter++;
-        printf("%d: %s\n", counter, line);
+        is_line_valid = strlen(line);
     }
     fclose(csv);
     return 0;
 }
+
