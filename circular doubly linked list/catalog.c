de964f30c83f56becaaacaf1b7de97772ba8fd25
nluizsoliveira <nluizsoliveira@gmail.com>
2021-11-03 07:34:28 -0300

    fix(circular doubly linked list): remove debug index printing

--- circular doubly linked list/catalog.c
+++ circular doubly linked list/catalog.c
@@ -49,7 +49,7 @@ void add_Game_to_Catalog(Game* game, Catalog* catalog){
 void print_Catalog(Catalog* catalog){
     Game* game = catalog->first;
     for(int counter = 0; counter < catalog->size; counter++) {
-        printf("%d %s\n", counter, game->name);
+        printf("%s\n",game->name);
         game = game->next;
     }
 }
