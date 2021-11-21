2dee6d70fc6f93939d61b4b15f9f86e0513e0f5e
nluizsoliveira <nluizsoliveira@gmail.com>
2021-11-21 20:58:58 -0300

    feat(binary tree): add csv line parsing, elements adding to tree and methods calling

--- binary tree/main.c
+++ binary tree/main.c
@@ -3,24 +3,42 @@
 #include<string.h>
 #include<cpf.h>
 #include<readline.h>
+#include<memory.h>
+#include<person.h>
+#include<tree.h>
 
 int main(){
     int clients_quantity;
     scanf("%d ", &clients_quantity);
 
+    Tree* tree = new_tree();
     for(int counter = 0; counter < clients_quantity; counter++){
-        char* client_info_string = read_line();
-        char* cpf_with_special_characters = (char*) malloc(strlen(client_info_string) * sizeof(char));
-        unsigned long long int cpf;
-        char* name = (char*) malloc(strlen(client_info_string) * sizeof(char));
+        const char PARSE_CSV_MASK[20] = "%[^;];%[^;];%d;%lf";
         int age;
         double balance;
+        unsigned long long int cpf;
 
-        sscanf(client_info_string, "%[^;];%[^;];%d;%lf", cpf_with_special_characters, name, &age, &balance);
+        char* client_info_string = read_line();
+        check_pointer(client_info_string);
+
+        char* cpf_with_special_characters = (char*) malloc(strlen(client_info_string) * sizeof(char));
+        check_pointer(cpf_with_special_characters);
+
+        char* name = (char*) malloc(strlen(client_info_string) * sizeof(char));
+        check_pointer(name);
+        
+        sscanf(client_info_string, PARSE_CSV_MASK, cpf_with_special_characters, name, &age, &balance);
         cpf = parse_cpf(cpf_with_special_characters);
-        printf("---%llu %s %d %lf ---\n", cpf, name, age, balance);
 
+        Person* person = new_person(cpf,name,age,balance);
+        insert(tree, person);
     }
+
+    in_order_transverse(tree);
+    pre_order_transverse(tree);
+    post_order_transverse(tree);
+
+    free_tree(tree);
 
     return 0;
 }
