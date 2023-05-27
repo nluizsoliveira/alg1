2f19ce6b50853ff477f8f8e63e07e2852debf25b
nluizsoliveira <nluizsoliveira@gmail.com>
2023-05-27 16:45:29 -0300

    test(file indexing): test appending and reading

--- 7 primary and secondary file indexing/test_file.py
+++ 7 primary and secondary file indexing/test_file.py
@@ -2,27 +2,30 @@ from file import File
 
 record_file = File('file')
 
-def test_append():
-    print('Test Append')
+def test_append_record():
+    print('Test Append Record')
     records = [
         (int('5'), 'The Diary of a Young Girl', 'Anne Frank'),
         (int('8'), 'The Fault in Our Stars', 'John Green'),
         (int('14'), 'Looking for Alaska', 'John Green'),
     ]
     for fields in records:
+        print('\t appending: ', fields)
         stream_size, pack_format = record_file.append_record(fields)
-        print(stream_size, pack_format)
+        print('\t appended:  ', stream_size, pack_format)
+        print('\t-----')
 
-def test_read_records():
-    print('Test Read Records')
+def test_read_at_position():
+    print('Test Read at position')
     sizes = [39, 36, 32]
     formats = ['i25s10s', 'i22s10s', 'i18s10s']
     start_positions = [0, 39, 39+36] # will be get from primary index
     
     for size, format, position in zip(sizes, formats, start_positions):
-        print(position, size, format)
+        print('\trecovering: ', position, size, format)
         fields = record_file.read_at_position(position, size, format)
-        print(fields)
+        print('\trecovered: ', fields)
+        print('\t-----')
 
-test_append()
-test_read_records()
+test_append_record()
+test_read_at_position()
