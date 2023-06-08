786b5de86182685961a9b45bec0050087a6a9985
nluizsoliveira <nluizsoliveira@gmail.com>
2023-06-08 12:35:00 -0300

    refactor(file indexers): remove unnecessary comments;moved to README

--- 6_primary_secondary_file_indexing/project/modules/record_file_and_indexers/primary_index.py
+++ 6_primary_secondary_file_indexing/project/modules/record_file_and_indexers/primary_index.py
@@ -1,5 +1,6 @@
 from project.modules.binary_handlers.file import File
 
+# Implementation Details: Read README.md
 class PrimaryIndex():
     SEP_1 = ','
     SEP_2 = '\n'
@@ -8,17 +9,18 @@ class PrimaryIndex():
         self.path = path
         self.file = File(self.path)
         self.RAM_index = self.get_RAM_index()
-    
-    # I'm just inserting/deleting from RAM_index
-    # Primary Index File will be updated when Orchestrator's 'EXIT' command calls PrimaryIndex.update_file
+
     def append(self, id_, position, stream_size, pack_format):
         is_duplicate = self.search(id_)
         if is_duplicate:
-            return -1
+            return None
         index_line = self.get_index_line(id_, position, stream_size, pack_format)
         id_, position, stream_size = self.cast_args_to_int((id_, position, stream_size))
-        self.RAM_index.update({id_: (position, stream_size, pack_format)})
-        return 1
+
+        appended_entry = {id_: (position, stream_size, pack_format)}
+        self.RAM_index.update(appended_entry)
+        
+        return appended_entry # warning. This is not a shallow copy. See https://onlinegdb.com/CDpj0Zz0O 
 
     def get_index_line(self, id_, start_position, stream_size, pack_format):
         return f'{id_}{self.SEP_1}{start_position}{self.SEP_1}{stream_size}{self.SEP_1}{pack_format}{self.SEP_2}'
@@ -36,7 +38,6 @@ class PrimaryIndex():
     def cast_args_to_int(args):
         return (int(arg) for arg in args)
         
-    # I'm using a dict/hashtable which has O(1) lookup, no sorting needed
     def get_RAM_index(self):
         if self.file.size: 
             return self.read_RAM_index_from_file()
@@ -57,8 +58,6 @@ class PrimaryIndex():
 
         return RAM_index
     
-    # I'm just inserting/deleting from RAM_index
-    # Primary file will be updated when Orchestrator's 'EXIT' command calls PrimaryIndex.update_file
     def remove(self, id_):
         compressed_record = self.search(id_)
         if compressed_record:
@@ -66,8 +65,8 @@ class PrimaryIndex():
             return compressed_record
         return None
 
-    # TO IMPROVE: make a method in File() that automatically writes a buffer
-    # and updates size. This should not be primary index responsibility but file itself.
+    # TO IMPROVE: make a method in File() that automatically overrides from buffer
+    # and updates size. This should not be index responsibility but file itself.
     def update_file(self):
         buffer = ""
         active_records =  len(self.RAM_index)
